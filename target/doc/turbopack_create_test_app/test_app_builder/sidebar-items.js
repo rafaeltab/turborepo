adeedd9a0ed05216a91430e891d8741deca3098f
rafaeltab <rafaeltab@users.noreply.github.com>
2023-01-21 15:28:34 +0000

    Update docs (89ead9b9d4a78b27c3eb100b4de580ed7ddd82c0)

---
+++ target/doc/turbopack_create_test_app/test_app_builder/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["EffectMode","How to run effects in components."]],"struct":[["PackageJsonConfig","Configuration struct to generate the `package.json` file of the test app."],["TestApp",""],["TestAppBuilder",""]]};
