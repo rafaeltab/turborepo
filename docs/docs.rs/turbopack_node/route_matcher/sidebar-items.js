74e0df67ef281a6336ac4d484f79b6d669a7a28f
rafaeltab <rafaeltab@users.noreply.github.com>
2023-02-21 20:42:18 +0000

    Update docs (24801f42c4a5249854d05f3cc788e7d9a3291796)

---
+++ docs/docs.rs/turbopack_node/route_matcher/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["Param",""]],"struct":[["ParamVc","Vc for [`Param`]"],["Params",""],["ParamsVc","Vc for [`Params`]"],["RouteMatcherVc",""]],"trait":[["RouteMatcher","Extracts parameters from a URL path."]],"type":[["ParamReadRef","see [turbo_tasks::ReadRef]"],["ParamsReadRef","see [turbo_tasks::ReadRef]"]]};
